aaf11f790c104e11a96378d89ec9598c6360f4bc
zhanghuan <1597508322@qq.com>
2021-11-11 23:38:51 +0800

    push- test

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello , hotfix");
         System.out.println("master-test");
         System.out.println("hotfix-test");
+        System.out.println("push-test");
     }
 }
